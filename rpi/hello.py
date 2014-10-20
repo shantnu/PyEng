1c567cdb733aac962d553759d7d584f992aafd34
shantnu <shantnu@users.noreply.github.com>
2014-10-20 14:06:18 +0100

    Adding routing with name

--- rpi/hello.py
+++ rpi/hello.py
@@ -6,4 +6,13 @@ app = Flask(__name__)
 def home():
     return "Hello World!"
 
-app.run(host='0.0.0.0', port=80, debug=True)
+@app.route("/<string:name>")
+def hello_name(name):
+    hello = "Hello " +  name
+    return hello
+
+# For the RPI    
+#app.run(host='0.0.0.0', port=80, debug=True)
+
+# For testing on a normal Pc
+app.run(debug=True)
